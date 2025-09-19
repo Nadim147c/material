fb481051fa036166fb138136f045eba7bebf1093
Ephemeral <theephemeral.txt@gmail.com>
2025-09-19 16:50:52 +0600

    feat(quantizer): add context support for celebi

--- quantizer/celebi.go
+++ quantizer/celebi.go
@@ -1,10 +1,36 @@
 package quantizer
 
+import "context"
+
 func QuantizeCelebi(input pixels, maxColor int) QuantizedMap {
-	wu := QuantizeWu(input, maxColor*5)
+	qm, _ := QuantizeCelebiWithContext(context.Background(), input, maxColor)
+	return qm
+}
+
+func QuantizeCelebiWithContext(ctx context.Context, input pixels, maxColor int) (QuantizedMap, error) {
+	if ctx.Err() != nil {
+		return nil, ctx.Err()
+	}
+
+	wu, err := QuantizeWuWithContext(ctx, input, maxColor*5)
+	if err != nil {
+		return nil, err
+	}
+
 	colors := make(pixelsLab, len(wu))
 	for i, c := range wu {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
 		colors[i] = c.ToLab()
 	}
-	return QuantizeWsMeans(input, colors, maxColor)
+
+	qm, err := QuantizeWsMeansWithContext(ctx, input, colors, maxColor)
+	if err != nil {
+		return nil, err
+	}
+
+	return qm, nil
 }
