64443b4487120ed6192a4f90b0adb2e022cc096a
Ephemeral <theephemeral.txt@gmail.com>
2025-11-02 12:42:28 +0600

    docs(dynamic): add idiomatic docs comment

--- dynamic/contrast_curve.go
+++ dynamic/contrast_curve.go
@@ -2,25 +2,22 @@ package dynamic
 
 import "github.com/Nadim147c/material/v2/num"
 
-// ContrastCurve represents a curve that provides contrast values based on
-// contrast level
+// ContrastCurve represents a curve that provides contrast values for different
+// contrast levels.
 type ContrastCurve struct {
 	low, normal, medium, high float64
 }
 
-// NewContrastCurve creates a new NewContrastCurve with the specified values
-// low: Value for contrast level -1.0
-// normal: Value for contrast level 0.0
-// medium: Value for contrast level 0.5
-// high: Value for contrast level 1.0
+// NewContrastCurve returns a new ContrastCurve with the given values for each
+// contrast level. The low value is used for contrast level -1.0, normal for
+// 0.0, medium for 0.5, and high for 1.0.
 func NewContrastCurve(low, normal, medium, high float64) *ContrastCurve {
 	return &ContrastCurve{low, normal, medium, high}
 }
 
-// Get returns the value at a given contrast level
-// contrast: The contrast level. 0.0 is the default (normal); -1.0 is the
-// lowest; 1.0 is the highest.
-// return: The value. For contrast ratios, a number between 1.0 and 21.0.
+// Get returns the value at the given contrast level. Contrast level 0.0 is the
+// default (normal), -1.0 is the lowest, and 1.0 is the highest. The returned
+// value for contrast ratios is between 1.0 and 21.0.
 func (c *ContrastCurve) Get(contrast float64) float64 {
 	if contrast <= -1.0 {
 		return c.low
