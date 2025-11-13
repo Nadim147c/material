f3ce5fcc584a4f08b739a8d3c88726faba521129
Ephemeral <theephemeral.txt@gmail.com>
2025-11-13 23:23:23 +0600

    docs(core): add go doc example for Generate

--- material_test.go
+++ material_test.go
@@ -1,6 +1,22 @@
 package material
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/Nadim147c/material/v2/color"
+)
+
+func ExampleGenerate() {
+	c := color.ARGBFromHexMust("#FF0000")
+	colors, _ := Generate(
+		FromColor(c),
+		WithDark(true),
+		WithContrast(0.5),
+		WithVariant(VariantExpressive),
+	)
+	fmt.Println(colors.Primary)
+}
 
 func TestGenerate(t *testing.T) {
 	pixels := []string{"#FF1100", "#11FF00", "#1111FF", "#3F3F11", "#007755"}
